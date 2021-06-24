c6268ec4f3e73588d5e4be985f1ad2b11668b8c0
Ryan Lawrence <volpanicstudios@gmail.com>
2021-06-24 11:13:25 +1000

    Ryan - Added Power Detection scripts and Scriptable object events.

---
+++ Isolated Control/Assets/Scripts/PowerManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct PowerRing
+    {
+        public List<PowerDetection> DetectionsInCircut;
+    }
+
+    public List<PowerRing> PowerRings;
+
+    public int PowerRingActive { get { return activePowerRing; }  set { UpdatePowerRing(value); }}
+    private int activePowerRing = 0;
+
+    private void Start()
+    {
+        if (PowerRings != null)
+        {
+            for(int i = 0; i < PowerRings.Count; i++)
+            {
+                if (activePowerRing == i) ActivateRing(PowerRings[i]);
+                else DeactivateRing(PowerRings[i]);
+            }
+        }
+    }
+
+
+    private void UpdatePowerRing(int value)
+    {
+        //Check if new value is in bounds
+        if (PowerRings == null || value >= PowerRings.Count || value < 0) return;
+
+        //Deactivate Current Ring
+        DeactivateRing(PowerRings[activePowerRing]);
+        ActivateRing(PowerRings[value]);
+        activePowerRing = value;
+    }
+
+    private void ActivateRing(PowerRing ring)
+    {
+        if (ring.DetectionsInCircut == null) return;
+        
+        for(int i = 0; i < ring.DetectionsInCircut.Count; i++)
+        {
+            ring.DetectionsInCircut[i].OnPowerActivate.Invoke();
+        }
+    }
+
+    private void DeactivateRing(PowerRing ring)
+    {
+        if (ring.DetectionsInCircut == null) return;
+
+        for (int i = 0; i < ring.DetectionsInCircut.Count; i++)
+        {
+            ring.DetectionsInCircut[i].OnPowerDeativate.Invoke();
+        }
+    }
+
+}
