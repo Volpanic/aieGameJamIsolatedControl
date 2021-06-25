d8ec26eb9571a43dfc9fe20182ba9aa8ebbc7cb4
Ryan Lawrence <volpanicstudios@gmail.com>
2021-06-25 14:38:14 +1000

    Ryan - added on trigger invoke

---
+++ Isolated Control/Assets/Scripts/OnTriggerInvoke.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerInvoke : MonoBehaviour
+{
+    public UnityEvent OnTriggerEntered;
+    public bool OnlyOnce = true;
+
+    public string TagMask = "";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (TagMask == "" || TagMask == other.tag)
+        {
+            OnTriggerEntered.Invoke();
+            if (OnlyOnce) enabled = false;
+        }
+    }
+}
